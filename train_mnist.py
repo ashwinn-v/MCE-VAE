ca7d9d877da00d58e8891578bce2856d2a270c61
ashwinn-v <52974732+ashwinn-v@users.noreply.github.com>
2022-01-05 02:30:20 +0530

    optuna

--- train_mnist.py
+++ train_mnist.py
@@ -1,3 +1,5 @@
+from numpy.lib.twodim_base import tri
+import optuna 
 import numpy as np
 import sys, os
 import torch
@@ -25,6 +27,7 @@ funcs:
                 return train_loss/c, train_reco_loss/c, train_div_var_tau/c, train_div_c/c
         
 '''
+
 
 def calc_loss(model, x, x_init, beta=1., n_sampel=4):
     # print('x is ', x.size())
@@ -181,8 +184,38 @@ def load_fold(n):
     return trans_loader, trans_test_loader
 
 
-def train(n,model, optim, num_epochs=20, 
+def train(n,params, num_epochs=20, 
           tr_mode='new', beta = 1.0):
+    c = '/content/'
+    transformation = str(args.transformation).lower()
+    
+    myTable = PrettyTable(['Fold', 'train_loss (ELBO)', 'train_RE (Reco Error)', 'train_div_var_tau (Disent KL)', 'train_div_c (Ent KL)','test_loss', 'test_RE', 'test_div_var_tau', 'test_div_c']) 
+    
+    in_size = aug_dim = 28*28
+    mode = transformation.upper()
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    tag = str(args.tag)
+    model = MCEVAE(in_size=in_size,
+                    aug_dim=aug_dim,
+                    latent_z_c=int(args.nCat),
+                    latent_z_var=int(args.nVar),
+                    mode=mode, 
+                    invariance_decoder='gated', 
+                    rec_loss=str(args.loss_type), 
+                    div='KL',
+                    in_dim=1, 
+                    out_dim=1, 
+                    hidden_z_c=params['hidden_z_c'],
+                    hidden_z_var=params['hidden_z_var'],
+                    hidden_tau=params['hidden_tau'], 
+                    activation=nn.Sigmoid,
+                    training_mode=str(args.training_mode),
+                    device = device,
+                    tag = tag).to(device)
+    lr = 1e-3
+    optim = torch.optim.Adam(model.parameters(), lr=lr)
     
     train_data, test_data = load_fold(n)
     
@@ -232,13 +265,32 @@ def train(n,model, optim, num_epochs=20,
             state = {'epoch': epoch + 1,
                      'state_dict': model.state_dict(),
                      'optimizer': optim.state_dict()}
-            torch.save(state, 'models/' + modelname)
-    print('saving...')
-    np.save('losses/trainloss_' + modelname.replace("_checkpoint", ""), train_loss_record)
-    np.save('losses/testloss_' + modelname.replace("_checkpoint", ""), test_loss_record)
+    #         torch.save(state, 'models/' + modelname)
+    # print('saving...')
+    # np.save('losses/trainloss_' + modelname.replace("_checkpoint", ""), train_loss_record)
+    # np.save('losses/testloss_' + modelname.replace("_checkpoint", ""), test_loss_record)
     ptt=[n,train_loss, train_RE, train_div_var_tau, train_div_c,test_loss, test_RE, test_div_var_tau, test_div_c]
     myTable.add_row(ptt)
+    return train_loss_record, test_loss_record
 
+def objective(trial):
+    narr = [1,2,3,4]
+    params = {
+        "hidden_z_c": trial.suggest_int("hidden_z_c",200,512),
+        "hidden_z_var": trial.suggest_int("hidden_z_c",200,512),
+        "hidden_tau": trial.suggest_int("hidden_z_c",32,128)
+    }
+    all_train_losses = []
+    all_test_losses = []
+    for i in narr:
+        print("Fold ", i)
+        temp_train_loss, test_loss_record = train(i,params, save_model=False)
+        all_train_losses.append(temp_train_loss)
+        all_test_losses.append(test_loss_record)
+    return np.mean(all_train_losses), np.mean(all_test_losses)
+
+
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -248,51 +300,37 @@ if __name__ == '__main__':
     parser.add_argument("--nVar", help = "Number of Variational Latent Dimensions", default = 3)
     parser.add_argument("--nBatch", help = "Batch size", default = 100)
     parser.add_argument("--nEpochs", help = "Number of Epochs", default = 60)
-    parser.add_argument("--nHiddenCat", help = "Number of Nodes in Hidden Layers for Categorical Latent Space", default = 512)
-    parser.add_argument("--nHiddenVar", help = "Number of Nodes in Hidden Layers for Variational Latent Space", default = 512)
-    parser.add_argument("--nHiddenTrans", help = "Number of Nodes in Hidden Layers for Transformational Latent Space", default = 32)
+    # parser.add_argument("--nHiddenCat", help = "Number of Nodes in Hidden Layers for Categorical Latent Space", default = 512)
+    # parser.add_argument("--nHiddenVar", help = "Number of Nodes in Hidden Layers for Variational Latent Space", default = 512)
+    # parser.add_argument("--nHiddenTrans", help = "Number of Nodes in Hidden Layers for Transformational Latent Space", default = 32)
     parser.add_argument("--tag", help = "tag for model name", default = "default")
     parser.add_argument("--training_mode", help = "Training mode: use supervised or unsupervised", default = "supervised")
     parser.add_argument("--beta", help = "Beta for beta-VAE training", default = 1.0)    
     parser.add_argument("--fold", help = "k fold number", default = 1)   
     args = parser.parse_args()
+    
 
-    print('loading data...')
-    c = '/content/'
+
     transformation = str(args.transformation).lower()
-    narr = [1,2,3,4]
+    
     myTable = PrettyTable(['Fold', 'train_loss (ELBO)', 'train_RE (Reco Error)', 'train_div_var_tau (Disent KL)', 'train_div_c (Ent KL)','test_loss', 'test_RE', 'test_div_var_tau', 'test_div_c']) 
     
     in_size = aug_dim = 28*28
     mode = transformation.upper()
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    tag = str(args.tag)
-    model = MCEVAE(in_size=in_size,
-                    aug_dim=aug_dim,
-                    latent_z_c=int(args.nCat),
-                    latent_z_var=int(args.nVar),
-                    mode=mode, 
-                    invariance_decoder='gated', 
-                    rec_loss=str(args.loss_type), 
-                    div='KL',
-                    in_dim=1, 
-                    out_dim=1, 
-                    hidden_z_c=int(args.nHiddenCat),
-                    hidden_z_var=int(args.nHiddenVar),
-                    hidden_tau=int(args.nHiddenTrans), 
-                    activation=nn.Sigmoid,
-                    training_mode=str(args.training_mode),
-                    device = device,
-                    tag = tag).to(device)
-    lr = 1e-3
-    optim = torch.optim.Adam(model.parameters(), lr=lr)
-    train(n = args.fold,model = model,
-        optim = optim, 
-        num_epochs = int(args.nEpochs), 
-        tr_mode='new',
-        beta = float(args.beta))
-    print(myTable)
+    study = optuna.create_study(direction=['minimize', 'minimize'])
+    study.optimize(objective, n_trials=10)
+    print('best trial:')
+    trial_ = study.best_trial
+    print('values',trial_.values)
+    print('params',trial_.params)
+    scores = 0
+    for i in range(1,5):
+        print('Fold',i)
+        scr = train(i,trial_.params, save_model=True)
+        scores += scr
+    print('Average scores',scores/4)  
 
 
 
